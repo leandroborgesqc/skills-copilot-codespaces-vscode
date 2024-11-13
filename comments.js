b516888a939aa74f4f5528d1279183248d261070
Leandro Borges <155820450+leandroborgesqc@users.noreply.github.com>
2024-11-13 11:20:52 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Create Web server
+// Create a function that will be called when a request is made to the server
+// The function will return a response with the comments as the body
+// Start the server on port 3000
+const http = require('http');
+const comments = require('./comments');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(comments.getComments());
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('Server running on http://localhost:3000');
+});
